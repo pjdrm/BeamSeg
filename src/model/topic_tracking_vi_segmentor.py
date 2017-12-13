af8e27d28efa2f8de8db1334c4f61cfca4f32908
pjdrm <pjdrm@LindaMartini>
2017-12-13 16:51:14 +0000

    WIP function that implements CVB iterations.

--- src/model/topic_tracking_vi_segmentor.py
+++ src/model/topic_tracking_vi_segmentor.py
@@ -7,11 +7,13 @@ import numpy as np
 from dataset.synthetic_doc import SyntheticTopicTrackingDoc
 from debug.synthetic_corpus_debugger import print_corpus
 from scipy.special import gamma, digamma
+import numpy_groupies as npg
 
 class TopicTrackingVIModel(object):
 
     def __init__(self, alpha, beta, K, doc):
         self.beta = beta
+        self.beta_sum = np.sum(self.beta)
         self.alpha = alpha
         self.K = K
         self.W = doc.W
@@ -23,6 +25,7 @@ class TopicTrackingVIModel(object):
         '''
         self.sents_len = self.doc.sents_len
         self.n_sents = self.doc.n_sents
+        self.n_words = np.sum(self.sents_len)
         
         '''
         Variational Parameters
@@ -38,11 +41,43 @@ class TopicTrackingVIModel(object):
         Initialization cannot be uniform, we draw multinomials
         from a Dirichlet to initialize each gamma_i.
         '''
-        n_words = np.sum(self.sents_len)
-        gamma_q = np.zeros((n_words, self.K))
-        for i in range(n_words):
+        gamma_q = np.zeros((self.n_words, self.K))
+        for i in range(self.n_words):
             gamma_q[i] = np.random.dirichlet(self.alpha)
         return gamma_q
+    
+    def cvb_iter(self):
+        '''
+        Performs a single iteration of the
+        Collapsed Variational Bayes algorithm. That is,
+        performs one round of updates for all variational 
+        parameters.
+        '''
+        for i in range(self.n_words):
+            # I think the updates are dependant on each other, we need to do one at a time
+            E_q_zi_k = np.sum(self.gamma_q, axis=0)-self.gamma_q[i]
+            Var_q_zi_k = E_q_zi_k*(1.0-E_q_zi_k)
+            
+            
+            gamma_q_all_wi = self.gamma_q[self.all_wi_dic[self.wi_vocab_index[i]]]
+            #TODO: self.all_wi_dic has keys vocab indexes and values the set of word indexes of that type
+            #TODO: self.wi_vocab_index has keys word indexes and returns the corresponding vocab index
+            q_wi_k = np.sum(gamma_q_all_wi, axis=0)
+            E_q_wi_k = (q_wi_k-self.gamma_q)*self.W_I_counts_minus1
+            Var_q_wi_k = E_q_wi_k*(1.0-E_q_wi_k)
+            
+            q_wi_k_plus_beta = q_wi_k + self.beta
+            E_q_wi_k_plus_beta = (q_wi_k_plus_beta[self.wi_vocab_index]-self.gamma_q)*self.W_I_counts_minus1
+            
+            f1 = E_q_zi_k + self.alpha
+            f2 = E_q_wi_k_plus_beta
+            f3 = 1.0/(E_q_zi_k+self.beta_sum)
+            f4 = Var_q_zi_k/(2.0*f1)**2
+            f5 = Var_q_wi_k/(2.0*f2)**2
+            f6 = f1/(2.0*(E_q_zi_k+self.beta_sum))**2
+            
+            self.gamma_q[i] = f1*f2*f3*np.e(-f4-f5+f6)
+        
         
 pi = 0.2
 alpha = 15
