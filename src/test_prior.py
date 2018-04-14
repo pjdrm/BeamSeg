3f46837c87b5249aa58f10b34fe73f6473f18022
root <root@PedroMint>
2018-04-14 16:58:43 +0100

    Bug fix + to - in update alpha.

--- src/test_prior.py
+++ src/test_prior.py
@@ -17,19 +17,17 @@ def draw_theta_topics(beta_t, theta_t_mean, K):
 
 def draw_words(K, W, topic_draws, theta_t):
     N_t_k_w = np.zeros((K, W))
-    word_counts_t = np.zeros(W)
     for k in range(K):
         for z in range(topic_draws[k]):
             word = np.random.multinomial(1, theta_t[k])
             wi = np.nonzero(word)[0][0] #w is a vocabulary index
-            word_counts_t[wi] += 1.0
             N_t_k_w[k, wi] += 1.0
            
     return N_t_k_w
 
 def update_alpha(phi_t_mean, topic_draws, alpha_t):
     n_draws = np.sum(topic_draws)
-    num_alpha = np.sum(phi_t_mean*(scipy.digamma(topic_draws+alpha_t*phi_t_mean)+scipy.digamma(alpha_t*phi_t_mean)), axis=0)
+    num_alpha = np.sum(phi_t_mean*(scipy.digamma(topic_draws+alpha_t*phi_t_mean)-scipy.digamma(alpha_t*phi_t_mean)), axis=0)
     denom_alpha = scipy.digamma(n_draws+alpha_t)-scipy.digamma(alpha_t)
     alpha_t_update = alpha_t*(num_alpha/denom_alpha)
     return alpha_t_update
@@ -55,13 +53,18 @@ def update_theta(N_t_k_w_k, beta_t_k, theta_t_mean_k):
     theta_t_mean_update = num_theta_t_mean/denom_theta_t_mean
     return theta_t_mean_update
     
+use_seed = False
+seed = 229#84
+if use_seed:
+    np.random.seed(seed)
+        
 n_draws = 100
 W = 10
 K = 5
-n_chunks = 15
-base_prior = 0.2
-alpha_t = base_prior
-phi_t_mean = np.array([1.0]*K)
+n_chunks = 20
+base_prior = 10
+alpha_t = 12
+phi_t_mean = np.random.dirichlet([base_prior]*K)
 
 beta_t = [base_prior]*K
 theta_t_mean = [np.array([1.0]*W) for k in range(K)]
@@ -69,6 +72,7 @@ theta_t = draw_theta_topics(beta_t, theta_t_mean, K)
 
 all_topic_prop_draws = []
 for t in range(n_chunks):
+    print(phi_t_mean)
     phi_t = np.random.dirichlet(alpha_t*phi_t_mean)
     all_topic_prop_draws.append(phi_t)
     topic_draws = np.random.multinomial(n_draws, phi_t, size=1)[0]
@@ -78,7 +82,6 @@ for t in range(n_chunks):
     
     alpha_t = update_alpha(phi_t_mean, topic_draws, alpha_t)
     phi_t_mean = update_phi(topic_draws, alpha_t, phi_t_mean)
-
 
     for k, beta in enumerate(beta_t):
         beta_t[k] = update_beta(theta_t_mean[k], N_t_k_w[k], beta)
