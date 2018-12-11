f477ef1a28122140e6ccddfe723f56077e78c514
pjdrm <pjdrm@PedroMint>
2018-12-11 11:51:51 +0000

    Bug fix.

--- src/process_results_script.py
+++ src/process_results_script.py
@@ -34,7 +34,11 @@ def get_domain_results(dir, domain):
         if domain in res_f:
             domain_id = res_f.split("_seg")[0]
             seg_type = "seg"+res_f.split("_seg")[1][0:2]
-            prior_type = res_f.split("modality_")[1].replace(".txt", "")
+            if "modality_" in res_f:
+                split_str = "modality_"
+            else:
+                split_str = "dataset_"
+            prior_type = res_f.split(split_str)[1].replace(".txt", "")
             
             if domain_id not in results_dict:
                 results_dict[domain_id] = {"segbl": {}, "segtt": {}}
@@ -54,10 +58,10 @@ def print_domain_results(results_dict):
             for doc in results_dict[domains_sort[0]]["segtt"]["gp"]["doc_names"]:
                 print_str += doc+"\t"
             print_str += "\nRND Segs\t"
-            for wd in results_dict[domains_sort[1]]["segtt"]["gp"]["wd_rnd_segs"]:
+            for wd in results_dict[domains_sort[0]]["segtt"]["gp"]["wd_rnd_segs"]:
                 print_str += str(wd)+"\t"
             print_str += "\nNO segs\t"
-            for wd in results_dict[domains_sort[1]]["segtt"]["gp"]["wd_bl_no_segs"]:
+            for wd in results_dict[domains_sort[0]]["segtt"]["gp"]["wd_bl_no_segs"]:
                 print_str += str(wd)+"\t"
             print_str += "\n\n"
             header = False
@@ -69,7 +73,7 @@ def print_domain_results(results_dict):
                 for wd in results_dict[sub_domain]["segbl"][prior_type]["wd"]:
                     print_str += str(wd)+"\t"
             else:
-                incomplete_domains.append(sub_domain+" segbl")
+                incomplete_domains.append(sub_domain)
             print_str += "\n"
         print_str += "\n"
         
@@ -80,13 +84,13 @@ def print_domain_results(results_dict):
                 for wd in results_dict[sub_domain]["segtt"][prior_type]["wd"]:
                     print_str += str(wd)+"\t"
             else:
-                incomplete_domains.append(sub_domain+" segtt")
+                incomplete_domains.append(sub_domain)
             print_str += "\n"
         print_str += "\n"
         header = True
     print(print_str)
     print(set(incomplete_domains))
     
-results_dict = get_domain_results("/home/pjdrm/Desktop/final_results", "bio_d11")
+results_dict = get_domain_results("/home/pjdrm/Desktop/final_results", "bio")
 print_domain_results(results_dict)
 #print(json.dumps(results_dict, sort_keys=True, indent=4))
