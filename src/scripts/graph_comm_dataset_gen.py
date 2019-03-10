336b7ff3d1304092c888941193a8e25e6fa29547
pjdrm <pjdrm@PedroMint>
2019-03-10 18:26:46 +0000

    Generating run script for graph topic identification.

--- src/scripts/graph_comm_dataset_gen.py
+++ src/scripts/graph_comm_dataset_gen.py
@@ -40,6 +40,46 @@ def find_best_params(results_file):
                                  "weight": None,
                                  "score_func": None,
                                  "n": None},
+                   "louvain": {"best_ari": -1,
+                                 "weight": None,
+                                 "score_func": None,
+                                 "n": None},
+                   "cnm": {"best_ari": -1,
+                                 "score_func": None,
+                                 "n": None},
+                   "bigclam": {"best_ari": -1,
+                                 "score_func": None,
+                                 "n": None},
+                   "clique_precolation": {"best_ari": -1,
+                                 "score_func": None,
+                                 "n": None},
+                   "fast_greedy": {"best_ari": -1,
+                                   "weight": None,
+                                 "score_func": None,
+                                 "n": None},
+                   "edge_betweeness": {"best_ari": -1,
+                                   "weight": None,
+                                 "score_func": None,
+                                 "n": None},
+                   "girvan_newman": {"best_ari": -1,
+                                 "score_func": None,
+                                 "n": None},
+                   "label_propagation": {"best_ari": -1,
+                                   "weight": None,
+                                 "score_func": None,
+                                 "n": None},
+                   "leading_eigenvector": {"best_ari": -1,
+                                   "weight": None,
+                                 "score_func": None,
+                                 "n": None},
+                   "spinglass": {"best_ari": -1,
+                                   "weight": None,
+                                 "score_func": None,
+                                 "n": None},
+                   "LDA": {"best_ari": -1,
+                                   "wordsPerTopic": None,
+                                 "score_func": None,
+                                 "n": None},
                     "Affinity": {"best_ari": -1,
                                  "damping": None,
                                  "preference": None},
@@ -55,7 +95,8 @@ def find_best_params(results_file):
                              "var": None,
                              "metric": None},
                     "Mean": {"best_ari": -1,
-                             "bandwidth": None}}
+                             "bandwidth": None},
+                   "kmeans": {"best_ari": -1}}
     with open(results_file) as f:
         lins = f.readlines()
     for l in lins:
@@ -72,6 +113,86 @@ def find_best_params(results_file):
             best_params[algo]["best_ari"] = ari
             best_params[algo]["steps"] = steps
             best_params[algo]["weight"] = weight
+            best_params[algo]["score_func"] = score_func
+            best_params[algo]["n"] = n
+        elif algo == "louvain":
+            weight = get_val(l, "weight: ")
+            score_func = get_val(l, "score_func: ")
+            n = get_val(l, "top-N ")
+            best_params[algo]["best_ari"] = ari
+            best_params[algo]["weight"] = weight
+            best_params[algo]["score_func"] = score_func
+            best_params[algo]["n"] = n
+        elif algo == "cnm":
+            score_func = get_val(l, "score_func: ")
+            n = get_val(l, "top-N ")
+            best_params[algo]["best_ari"] = ari
+            best_params[algo]["score_func"] = score_func
+            best_params[algo]["n"] = n
+        elif algo == "bigclam":
+            score_func = get_val(l, "score_func: ")
+            n = get_val(l, "top-N ")
+            best_params[algo]["best_ari"] = ari
+            best_params[algo]["score_func"] = score_func
+            best_params[algo]["n"] = n
+        elif algo == "clique_precolation":
+            score_func = get_val(l, "score_func: ")
+            n = get_val(l, "top-N ")
+            best_params[algo]["best_ari"] = ari
+            best_params[algo]["score_func"] = score_func
+            best_params[algo]["n"] = n
+        elif algo == "fast_greedy":
+            weight = get_val(l, "weight: ")
+            score_func = get_val(l, "score_func: ")
+            n = get_val(l, "top-N ")
+            best_params[algo]["weight"] = weight
+            best_params[algo]["best_ari"] = ari
+            best_params[algo]["score_func"] = score_func
+            best_params[algo]["n"] = n
+        elif algo == "edge_betweeness":
+            weight = get_val(l, "weight: ")
+            score_func = get_val(l, "score_func: ")
+            n = get_val(l, "top-N ")
+            best_params[algo]["weight"] = weight
+            best_params[algo]["best_ari"] = ari
+            best_params[algo]["score_func"] = score_func
+            best_params[algo]["n"] = n
+        elif algo == "girvan_newman":
+            score_func = get_val(l, "score_func: ")
+            n = get_val(l, "top-N ")
+            best_params[algo]["best_ari"] = ari
+            best_params[algo]["score_func"] = score_func
+            best_params[algo]["n"] = n
+        elif algo == "label_propagation":
+            weight = get_val(l, "weight: ")
+            score_func = get_val(l, "score_func: ")
+            n = get_val(l, "top-N ")
+            best_params[algo]["weight"] = weight
+            best_params[algo]["best_ari"] = ari
+            best_params[algo]["score_func"] = score_func
+            best_params[algo]["n"] = n
+        elif algo == "leading_eigenvector":
+            weight = get_val(l, "weight: ")
+            score_func = get_val(l, "score_func: ")
+            n = get_val(l, "top-N ")
+            best_params[algo]["weight"] = weight
+            best_params[algo]["best_ari"] = ari
+            best_params[algo]["score_func"] = score_func
+            best_params[algo]["n"] = n
+        elif algo == "spinglass":
+            weight = get_val(l, "weight: ")
+            score_func = get_val(l, "score_func: ")
+            n = get_val(l, "top-N ")
+            best_params[algo]["weight"] = weight
+            best_params[algo]["best_ari"] = ari
+            best_params[algo]["score_func"] = score_func
+            best_params[algo]["n"] = n
+        elif algo == "LDA":
+            wordsPerTopic = get_val(l, "wordsPerTopic: ")
+            score_func = get_val(l, "score_func: ")
+            n = get_val(l, "top-N ")
+            best_params[algo]["wordsPerTopic"] = wordsPerTopic
+            best_params[algo]["best_ari"] = ari
             best_params[algo]["score_func"] = score_func
             best_params[algo]["n"] = n
         elif algo == "Affinity":
@@ -99,6 +220,15 @@ def find_best_params(results_file):
             bandwidth = get_val(l, "bandwidth: ")
             best_params[algo]["best_ari"] = ari
             best_params[algo]["bandwidth"] = bandwidth
+        elif algo == "dbscan":
+            minPts = get_val(l, "minPts: ")
+            eps = get_val(l, "eps: ")
+            metric = get_val(l, "metric: ")
+            best_params[algo]["best_ari"] = ari
+            best_params[algo]["minPts"] = minPts
+            best_params[algo]["eps"] = eps
+            best_params[algo]["metric"] = metric
+            best_params[algo]["vars"] = var
     return best_params
 
 def gen_config(best_params, cfg_template_path):
@@ -113,10 +243,67 @@ def gen_config(best_params, cfg_template_path):
             algo_cfg["weight_schemes"] = [algo_params["weight"]]
             algo_cfg["score_function"] = [algo_params["score_func"]]
             algo_cfg["steps"] = '['+algo_params["steps"]+']'
+        elif algo == "louvain":
+            algo_cfg = cfg_template["algorithms"]["slicing_louvain"]
+            algo_cfg["n"] = '['+algo_params["n"]+']'
+            algo_cfg["weight_schemes"] = [algo_params["weight"]]
+            algo_cfg["score_function"] = [algo_params["score_func"]]
+        elif algo == "cnm":
+            algo_cfg = cfg_template["algorithms"]["slicing_cnm"]
+            algo_cfg["n"] = '['+algo_params["n"]+']'
+            algo_cfg["score_function"] = [algo_params["score_func"]]
+        elif algo == "bigclam":
+            algo_cfg = cfg_template["algorithms"]["slicing_bigclam"]
+            algo_cfg["n"] = '['+algo_params["n"]+']'
+            algo_cfg["score_function"] = [algo_params["score_func"]]
+        elif algo == "clique_precolation":
+            algo_cfg = cfg_template["algorithms"]["slicing_clique_precolation"]
+            algo_cfg["n"] = '['+algo_params["n"]+']'
+            algo_cfg["score_function"] = [algo_params["score_func"]]
+        elif algo == "fast_greedy":
+            algo_cfg = cfg_template["algorithms"]["slicing_community_fastgreedy"]
+            algo_cfg["n"] = '['+algo_params["n"]+']'
+            algo_cfg["weight_schemes"] = [algo_params["weight"]]
+            algo_cfg["score_function"] = [algo_params["score_func"]]
+        elif algo == "edge_betweeness":
+            algo_cfg = cfg_template["algorithms"]["slicing_edge_betweenness"]
+            algo_cfg["n"] = '['+algo_params["n"]+']'
+            algo_cfg["weight_schemes"] = [algo_params["weight"]]
+            algo_cfg["score_function"] = [algo_params["score_func"]]
+        elif algo == "girvan_newman":
+            algo_cfg = cfg_template["algorithms"]["slicing_girvan_newman"]
+            algo_cfg["n"] = '['+algo_params["n"]+']'
+            algo_cfg["score_function"] = [algo_params["score_func"]]
+        elif algo == "label_propagation":
+            algo_cfg = cfg_template["algorithms"]["slicing_label_propagation"]
+            algo_cfg["n"] = '['+algo_params["n"]+']'
+            algo_cfg["weight_schemes"] = [algo_params["weight"]]
+            algo_cfg["score_function"] = [algo_params["score_func"]]
+        elif algo == "leading_eigenvector":
+            algo_cfg = cfg_template["algorithms"]["slicing_leading_eigenvector"]
+            algo_cfg["n"] = '['+algo_params["n"]+']'
+            algo_cfg["weight_schemes"] = [algo_params["weight"]]
+            algo_cfg["score_function"] = [algo_params["score_func"]]
+        elif algo == "spinglass":
+            algo_cfg = cfg_template["algorithms"]["slicing_spinglass"]
+            algo_cfg["n"] = '['+algo_params["n"]+']'
+            algo_cfg["weight_schemes"] = [algo_params["weight"]]
+            algo_cfg["score_function"] = [algo_params["score_func"]]
+        elif algo == "LDA":
+            algo_cfg = cfg_template["algorithms"]["slicing_lda"]
+            algo_cfg["n"] = '['+algo_params["n"]+']'
+            algo_cfg["score_function"] = [algo_params["score_func"]]
+            algo_cfg["wordsPerTopic"] = '['+algo_params["wordsPerTopic"]+']'
         elif algo == "Affinity":
             algo_cfg = cfg_template["algorithms"]["slicing_affinity_propagation"]
             algo_cfg["damping"] = '['+algo_params["damping"]+']'
             algo_cfg["preference"] = '['+algo_params["preference"]+']'
+        elif algo == "dbscan":
+            algo_cfg = cfg_template["algorithms"]["slicing_dbscan"]
+            algo_cfg["metric"] = [algo_params["metric"]]
+            algo_cfg["eps"] = '['+algo_params["eps"]+']'
+            algo_cfg["minPts"] = '['+algo_params["minPts"]+']'
+            algo_cfg["vars"] = '['+algo_params["vars"]+']'
         elif algo == "Agglomerative":
             algo_cfg = cfg_template["algorithms"]["slicing_agglomerative_clustering"]
             algo_cfg["metric"] = [algo_params["metric"]]
@@ -125,7 +312,7 @@ def gen_config(best_params, cfg_template_path):
         elif algo == "Spectral":
             algo_cfg = cfg_template["algorithms"]["slicing_spectral"]
             algo_cfg["metric"] = [algo_params["metric"]]
-            algo_cfg["vars"] = '['+algo_params["vars"]+']'
+            algo_cfg["vars"] = '['+algo_params["var"]+']'
         elif algo == "Mean":
             algo_cfg = cfg_template["algorithms"]["slicing_mean_shift"]
             algo_cfg["bandwidth"] = '['+algo_params["bandwidth"]+']'
@@ -154,10 +341,13 @@ def gen_experiment(algs_cfg, yaml_fp, domains_dir, results_dir, domain_desc, out
     
     with open(yaml_fp) as f:
         yalm_template = f.read()
-        
+    
+    run_script = ""
     for domain_dir in os.listdir(domains_dir):
         if "domain" in domain_dir:
             domain_id = domain_dir.replace("omain", "")+"_"
+        else:
+            domain_id = domain_dir+"_"
         result_fp = get_res_fp(domain_id, domain_desc, results_dir)
         all_segment_label, all_doc_txt = gen_dataset(domains_dir+domain_dir+"/doc_segs", results_dir+result_fp)
         cfg = copy.deepcopy(algs_cfg)
@@ -172,18 +362,22 @@ def gen_experiment(algs_cfg, yaml_fp, domains_dir, results_dir, domain_desc, out
             docs_f.write(all_doc_txt)
             
         yalm_cfg = copy.deepcopy(yalm_template)
-        yalm_cfg = change_doc_in_dir(yalm_cfg, out_dir+"docs/"+domain_id[:-1])
+        yalm_cfg = change_doc_in_dir(yalm_cfg, "resources/"+domain_desc+"/docs/"+domain_id[:-1])
         yalm_fp = domain_desc+"_"+domain_dir+".yaml"
         with open(out_dir+"/yalms/"+yalm_fp, "w+") as docs_f:
             docs_f.write(yalm_cfg)
+            
+        run_script += "python tests/slice_tester.py resources/"+domain_desc+"/yalms/"+yalm_fp+" "+"resources/"+domain_desc+"/configs/"+cfg_fp+"\n"
+    with open(out_dir+"run_tests_"+domain_desc+".sh", "w+") as f:
+        f.write(run_script)
         
         
 best_params = find_best_params("/home/pjdrm/Dropbox/results_AVL_common_topics.txt")
 cfg_final = gen_config(best_params, "/home/pjdrm/Dropbox/tests_bio_d0.json")
 gen_experiment(cfg_final,
                "/home/pjdrm/Dropbox/bio_d0.yaml",
-               "/home/pjdrm/eclipse-workspace/TopicTrackingSegmentation/dataset/Biography/",
-               "/home/pjdrm/eclipse-workspace/TopicTrackingSegmentation/thesis_exp/beamseg/",
-               "bio",
-               "/home/pjdrm/Desktop/gcm_exps/Biography/")
+               "/home/pjdrm/workspace/TopicTrackingSegmentation/dataset/Lectures/",
+               "/home/pjdrm/workspace/TopicTrackingSegmentation/thesis_exp/beamseg/",
+               "lectures",
+               "/home/pjdrm/Desktop/gcm_exps/Lectures/")
 #gen_dataset("/home/pjdrm/eclipse-workspace/TopicTrackingSegmentation/dataset/Biography/domain0/doc_segs", "/home/pjdrm/eclipse-workspace/TopicTrackingSegmentation/thesis_exp/beamseg/bio_d0_segbl_modality_bb.txt")
